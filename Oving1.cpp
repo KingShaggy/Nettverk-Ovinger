987681f1beec1a9a24bdd9aff1babaca9f3e963a
Mikkel Ofrim <mikkel.ofrim@gmail.com>
2022-01-11 23:06:06 +0100

    Added everything except the thread handeling

--- Oving1.cpp
+++ Oving1.cpp
@@ -5,7 +5,38 @@
 
 using namespace std;
 
+bool check_if_prime(int num){
+    if (num == 0 || num == 1){
+        return false;
+    }
+    else {
+        for (int i = 2; i <= num / 2; ++i){
+            if (num % i == 0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    
+    vector<thread> threads;
+    vector<int> primes;
+
+    int start = 0;
+    int end = 100;
+
+    while(start < end){
+        if (check_if_prime(start)){
+            primes.push_back(start);
+        }
+        ++start;
+    }
+
+    for (auto i: primes){
+        cout << i << " ";
+    }
+
+    return 0;
 }
