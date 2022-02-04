4f7b320a8f491d9d2866bd2c5c0857fc8eb99102
Mikkel Ofrim <mikkel.ofrim@gmail.com>
2022-02-04 16:41:17 +0100

    Done

--- Oving2.cpp
+++ Oving2.cpp
@@ -4,52 +4,106 @@
 #include <condition_variable>
 #include <list>
 #include <mutex>
-
+#include <functional>
+#include <chrono>
+ 
 using namespace std;
-
+using namespace chrono;
+ 
 class Workers {
-private:
+private: 
     int numOfThreads;
     condition_variable cv;
     vector<thread> threads;
-    list<function<void>> tasks;
+    list<function<void()>> tasks;
     mutex tasks_mutex;
-
+    bool run = true;
+    bool waiting;
+ 
 public:
     Workers(int num) {
         numOfThreads = num;
     }
-
+ 
     void start() {
-        
+        for(int i = 0; i < numOfThreads; i++){
+            threads.emplace_back([this] {
+                while(run) {
+                    function<void()> task;
+                    {
+                        unique_lock<mutex> lock(tasks_mutex);
+                        while(waiting) {
+                            cv.wait(lock);
+                        }
+                        waiting = true;
+                        if(!tasks.empty()) {
+                            task = *tasks.begin();
+                            tasks.pop_front();
+                        }
+                        lock.unlock();
+                        waiting = false;
+                        cv.notify_one();
+                    }
+                    if(task) {
+                        task();
+                    }
+                }
+            });
+        }
+        cv.notify_one();
+    }
+ 
+    void post(function<void()> f) {
+        unique_lock<mutex> lock(tasks_mutex);
+        tasks.emplace_back(f);
+        lock.unlock();
+        waiting = false;
+        cv.notify_one();
+    }
+ 
+    void stop() {
+        run = false;
+        for(auto& t : threads){
+            t.join();
+        }
+    }
+ 
+    void post_timeout(const function<void()>& function, int t) {
+        this_thread::sleep_for(milliseconds (t));
+        unique_lock<mutex> lock(tasks_mutex);
+        tasks.emplace_back(function);
+        waiting = false;
+        cv.notify_one();
     }
 };
-
-int main()
-{
+ 
+int main() {
     Workers worker_threads(4);
     Workers event_loop(1);
-
-    worker_threads.start();
-    event_loop.start();
-
-
+ 
+    worker_threads.start(); 
+    event_loop.start(); 
+ 
     worker_threads.post([] {
-        cout << "Task A" << endl;
+        cout << "A" << endl;
     });
-
+ 
     worker_threads.post([] {
-        cout << "Task B" << endl;
+        cout << "B" << endl;
     });
-
+ 
     event_loop.post([] {
-        cout << "Task C" << endl;
+        cout << "C" << endl;
     });
-
+ 
     event_loop.post([] {
-        cout << "Task D" << endl;
+        cout << "D" << endl;
     });
 
-    worker_threads.join();
-    event_loop.join();
+    worker_threads.post_timeout([] {
+        cout << "Timeout thread_id: " << this_thread::get_id() << endl;
+    }, 1000);
+
+    worker_threads.stop();
+    event_loop.stop();
 }
