133ee83f41314ac26a63293edb87749bc30d11e6
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-03-03 21:07:58 +0100

    Can now write to file using the api call

--- oving5/Backend/src/main/java/com/nettverk/mikkelof/oving5/web/Oving5Controller.java
+++ oving5/Backend/src/main/java/com/nettverk/mikkelof/oving5/web/Oving5Controller.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 @RestController
 @CrossOrigin
 public class Oving5Controller {
     @PostMapping("/compileAndRun")
-    public String compileAndRun(@RequestBody String code){
-        System.out.println(code);
+    public String compileAndRun(@RequestBody String code) throws IOException {
+        FileWriter writer = new FileWriter("../resources/code.txt");
+        String codeToFile = code.substring(1, code.length() - 1);
+        writer.write(codeToFile);
+        writer.close();
         return(code);
     }
 }
